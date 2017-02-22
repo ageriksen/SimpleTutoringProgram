9095201bd9d5deca5f09f50219ab23094ee4e453
phantomphysicist <andersge92@gmail.com>
2017-02-22 20:43:11 +0100

    this should reset

--- STP/STP/MainWindow.xaml.vb
+++
@@ -1,9 +0,0 @@
-﻿Class MainWindow
-    Private Sub StartButton_Click(sender As Object, e As RoutedEventArgs) Handles StartButton.Click
-        Dim vid As Window
-        vid = New video()
-        vid.Show()
-        vid = Nothing
-        Close()
-    End Sub
-End Class
